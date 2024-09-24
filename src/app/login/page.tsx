18c187ed1f1c359a3855041ce03e3f9f2ecca8ac
Swann Gillé <swanngille@gmail.com>
2024-09-24 23:31:58 +0200

    ajout forms login / register

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/app/login/loginForm";
+
+export default function Login() {
+    return (
+        <div className="justify-center items-center flex flex-col">
+            <LoginForm />
+        </div>
+    )
+}
