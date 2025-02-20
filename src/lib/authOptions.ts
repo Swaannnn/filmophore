33bc8e0880894348379b5a6585ce29407af04520
swaannnn <swanngille@gmail.com>
2025-02-20 11:07:04 +0100

    fix authOptions

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,50 @@
+import {NextAuthOptions} from "next-auth";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import {prisma} from "@/lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            type: "credentials",
+            credentials: {
+                email: { label: "Email", type: "email", required: true },
+                password: { label: "Password", type: "password", required: true },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email },
+                });
+
+                if (!user) return null;
+
+                const isValid = await bcrypt.compare(credentials.password, user.password);
+                if (!isValid) return null;
+
+                return { ...user, username: user.username };
+            },
+        }),
+    ],
+    session: { strategy: "jwt" },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.username = user.username;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string;
+                session.user.username = token.username as string;
+            }
+            return session;
+        },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
