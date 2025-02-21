c0e002cddb1ae4e94656413f8ddb23636bfb45d6
swaannnn <swanngille@gmail.com>
2025-02-21 14:43:52 +0100

    fix migration prisma

---
+++ prisma/migrations/20250221134311_fix_name_movie_list/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the `MovieListCard` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "MovieListCard" DROP CONSTRAINT "MovieList_userId_fkey";
+
+-- DropTable
+DROP TABLE "MovieListCard";
+
+-- CreateTable
+CREATE TABLE "MovieList" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "moviesId" TEXT[],
+
+    CONSTRAINT "MovieList_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "MovieList" ADD CONSTRAINT "MovieList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
